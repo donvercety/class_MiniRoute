1043fa3970e990c53ae08229352063f6fd06706b
Tommy Vercety <info@donvercety.biz>
2014-12-07 13:52:19 +0200

    small rearangements

--- controllers/About.php
+++ controllers/About.php
@@ -5,8 +5,9 @@ class About {
         echo 'This is the about page. </br />';   
         $this->_other();
     }
-    
+
     protected function _other() {
-        echo 'This is the _other functionin about, lolz!';  
+        echo 'This is the _other functionin about, lolz!'; 
+        echo '<pre>', print_r($_GET, TRUE), '</pre>';
     }
 }
